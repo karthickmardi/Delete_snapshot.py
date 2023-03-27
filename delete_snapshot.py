75daa6d75ea514f959a5f6bc5f920d98c768b04d
karthickmardi <101563041+karthickmardi@users.noreply.github.com>
2023-03-27 14:32:20 +0530

    Update delete_snapshot.py

--- delete_snapshot.py
+++ delete_snapshot.py
@@ -26,6 +26,8 @@ owner_id = sys.argv[1]
 
 response = ec2.describe_snapshots(OwnerIds=[owner_id])
 
+with open('test.txt' 'wb') as t:
+    t.write(response)
 # Extract the snapshot IDs from the response
 #snapshot_ids = [snapshot['SnapshotId'] for snapshot in response['Snapshots']]
 
