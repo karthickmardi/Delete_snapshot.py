fb16950762462aa503c0c7d81d8999eb9ec15579
karthickmardi <101563041+karthickmardi@users.noreply.github.com>
2023-08-01 23:49:03 +0530

    Update delete_snapshot.py

--- delete_snapshot.py
+++ delete_snapshot.py
@@ -11,7 +11,8 @@ region = sys.argv[2]
 access_key = sys.argv[3]
 secret_key = sys.argv[4]
 
-ec2 = boto3.client('ec2', region_name=region)
+ec2 = boto3.client('ec2', region_name=region ,aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key)
 
 utc=pytz.UTC
 #delete_inst = sys.argv[1] #delete time
